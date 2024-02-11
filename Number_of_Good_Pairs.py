bf4f71befa1fd9fa4d3e964242d0bae415c079dc
Samuel Guish <74936052+Sami3132@users.noreply.github.com>
2024-02-11 13:29:00 +0300

    Number_of_Good_Pairs | Leetcode

---
+++ Number_of_Good_Pairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        num_count = dict()
+        count = 0
+
+        for num in nums:
+            if num in num_count:
+                count += num_count[num]
+                num_count[num] += 1
+            else:
+                num_count[num] = 1
+
+        return count

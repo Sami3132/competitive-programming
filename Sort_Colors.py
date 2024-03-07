cdc82990ac5817895b554056e4971968957b76f0
Samuel Guish <74936052+Sami3132@users.noreply.github.com>
2024-03-07 15:17:54 +0300

    Sort_Colors | LeetCode

---
+++ Sort_Colors.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums) - 1):
+            minNum = i
+
+            for j in range(i + 1, len(nums)):
+                if nums[j] < nums[minNum]:
+                    minNum = j
+            
+            if minNum != i:
+                nums[minNum], nums[i] = nums[i], nums[minNum]

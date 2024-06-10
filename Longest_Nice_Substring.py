c7f41e093a335f097e96ebad6f996a5669a59267
Samuel Guish <74936052+Sami3132@users.noreply.github.com>
2024-06-10 23:36:25 +0300

    Longest_Nice_Substring | LeetCode

---
+++ Longest_Nice_Substring.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        def divcon(s):    
+            if len(s) < 2:
+                return ""
+            
+            pivot = []
+            for i, ch in enumerate(s):
+                if ch.isupper() and ch.lower() not in s:
+                    pivot.append(i)
+                if ch.islower() and ch.upper() not in s:
+                    pivot.append(i)
+                    
+            if not pivot:
+                return s
+            else:
+                mid = (len(pivot)) // 2
+                return max(divcon(s[:pivot[mid]]),divcon(s[pivot[mid]+1:]),key=len)
+        
+        return divcon(s)

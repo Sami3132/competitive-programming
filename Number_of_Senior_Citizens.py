c7535f57b105ac23f828d3b62d1c35020adcf83e
Samuel Guish <74936052+Sami3132@users.noreply.github.com>
2024-08-16 23:58:25 +0300

    Number_of_Senior_Citizens | LeetCode

---
+++ Number_of_Senior_Citizens.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        count = 0
+
+        for passenger in details:
+            if int(passenger[11] + passenger[12]) > 60:
+                count += 1
+        
+        return count

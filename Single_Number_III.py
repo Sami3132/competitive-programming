ac10211d12b11b6965a94f67283dcce9f024caf8
Samuel Guish <74936052+Sami3132@users.noreply.github.com>
2024-09-17 01:51:52 +0300

    Single_Number_III | LeetCode

---
+++ Single_Number_III.py
@@ -0,0 +1,21 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+
+        for num in nums:
+            xor ^= num
+        
+        diffBit = 1
+
+        while not (diffBit & xor):
+            diffBit = diffBit << 1
+        
+        a, b = 0, 0
+
+        for num in nums:
+            if num & diffBit:
+                a = a ^ num
+            else:
+                b = b ^ num
+        
+        return [a, b]

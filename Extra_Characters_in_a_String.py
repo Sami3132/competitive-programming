4141523f6a6a5e9320277d571c816bd080840e3b
Samuel Guish <74936052+Sami3132@users.noreply.github.com>
2024-09-23 15:53:18 +0300

    Extra_Characters_in_a_String | LeetCode

---
+++ Extra_Characters_in_a_String.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        mySet = set(dictionary)
+        dp = { len(s): 0 }
+
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            
+            ans = 1 + dfs(i + 1)
+
+            for j in range(i, len(s)):
+                if s[i:j + 1] in mySet:
+                    ans = min(ans, dfs(j + 1))
+            
+            dp[i] = ans
+
+            return ans
+        
+        return dfs(0)

776a7a2dbe05231623770bd02c3beb147d007153
Samuel Guish <74936052+Sami3132@users.noreply.github.com>
2025-02-01 19:46:02 +0300

    Special_Array_I | LeetCode

---
+++ Special_Array_I.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        for i in range(len(nums) - 1):
+            if not nums[i] & 1 ^ nums[i + 1] & 1:
+                return False
+        
+        return True
